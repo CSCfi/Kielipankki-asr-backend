04d1d143dd81f6993090e25f9f038c2b70f868df
Sam Hardwick <sam.hardwick@csc.fi>
2023-10-30 17:09:37 +0200

    Switch to fetching / compiling model from Zenodo

--- install_model.sh
+++ install_model.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/bash
 
-MODEL_ZIP_URL="https://a3s.fi/lb-asr-models/lb-lp-baseline-kaldi-model.zip"
+ORIG_DIR=$(pwd)
+
+AM_URL="https://zenodo.org/records/7101543/files/am_lp_1600h.zip"
+EXTRACTOR_URL="https://zenodo.org/records/7101543/files/extractor.zip"
+GRAPH_MORFESSOR_LP_DSP_URL="https://zenodo.org/records/7101543/files/graph_morfessor_lp_web_dsp.zip"
+GRAPH_WORD_DSP_URL="https://zenodo.org/records/7101543/files/graph_word_lp_web_dsp.zip"
 
 if [[ -e kaldi-serve/model-fi ]]
 then
@@ -8,20 +13,39 @@ then
     exit 1
 fi
 
-echo "Fetching model from $MODEL_ZIP_URL"
-echo
+TMP_DIR=$(mktemp --directory)
+cd $TMP_DIR
 
-curl $MODEL_ZIP_URL --output model.zip
-if [ $? -ne 0 ]; then
-	echo "Failed to fetch model zip, curl returned error code $?"
-fi
+packages=($AM_URL $EXTRACTOR_URL $GRAPH_WORD_DSP_URL)
+for package in "${packages[@]}"; do
+    echo "Fetching $(basename $package) from Zenodo"
+    echo "***"
+    echo
+    wget $package
+    if [ $? -ne 0 ]; then
+	echo "Failed to fetch model zip, wget returned error code $?"
+	exit 1
+    fi
+    mkdir $(basename -s .zip $package)
+    echo
+    echo "***"
+    echo "Extracting"
+    unzip -q -d $(basename -s .zip $package) $(basename $package)
+done
 
-echo "Unpacking model..."
-unzip -q model.zip
 
 echo "Installing model to kaldi-serve/model-fi and model-spec.toml to kaldi-serve/..."
-mv model-spec.toml kaldi-serve/
-mv model-fi/ kaldi-serve/
-echo "Cleaning up..."
+
+mkdir model-fi model-fi/ivector_extractor
+mv am_lp_1600h/exp/swbd/chain/tdnn7q_sp/* model-fi
+mv extractor/exp/nnet3/extractor/* model-fi/ivector_extractor
+mv graph_word_lp_web_dsp/exp/swbd/chain/tdnn7q_sp/graph_word_lp_web_dsp_nosp/* model-fi
+
+mv model-fi $ORIG_DIR/kaldi-serve/model-fi
+cd $ORIG_DIR
+cp -r config/conf kaldi-serve/model-fi/
+cp config/model-spec.toml kaldi-serve/
+
+echo "Cleaning up"
+rm -rf $TMP_DIR
 echo "Done."
-rm model.zip
